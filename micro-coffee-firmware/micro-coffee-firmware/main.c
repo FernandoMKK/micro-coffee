075c11eafae87d971c36ac76c3c3d0d9a30e14f1
fernando <fernando.marcon.kirchner@gmail.com>
2022-06-20 20:26:03 -0300

    Adição inicial de arquivos ao Repositório
    Adiciona os arquivos inciais ao repositório. Essa versão não faz nada.

---
+++ micro-coffee-firmware/micro-coffee-firmware/main.c
@@ -0,0 +1,18 @@
+/*
+ * micro-coffee-firmware.c
+ *
+ * Created: 20/06/2022 20:24:02
+ * Author : Faculdade
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
